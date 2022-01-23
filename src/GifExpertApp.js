26e40192d03e2cd452dc2d77c13fdb58563f0b3b
wlmc <williammcastro@msn.com>
2022-01-23 15:46:34 -0500

    inicio GifApp

---
+++ src/GifExpertApp.js
@@ -0,0 +1,17 @@
+
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const GifExpertApp = (props) => {
+  return( 
+  
+  <div>
+      <h2>GifExpertApp</h2>
+  </div>
+  )
+};
+
+GifExpertApp.propTypes = {};
+
+export default GifExpertApp;
